83010a328395e30517c8b7579deb4af0e540b70d
Clark-SC <suncheng1996@aliyun.com>
2017-06-15 00:51:09 +0800

    canvas

--- code/game.js
+++ code/game.js
@@ -1,27 +1,17 @@
 //读取关卡
-function Level(plan){
-	this.width = plan[0].length;//地图宽度
-	this.height = plan.length;//地图高度
-	this.grid = [];//网格
+function Level(grid){
+	this.width = grid.size.width;//地图宽度
+	this.height = grid.size.height;//地图高度
+	this.grid = grid.cells;//网格
 	this.actors = [];//活动元素
-	for (var y = 0; y < this.height; y++) {
-		var line = plan[y], gridLine = [];
-		for (var x = 0; x < this.width; x++) {
-		  var ch = line[x], fieldType = null;
-		  var Actor = actorChars[ch];
+	
+	grid.eachCell(function(x, y, ch){
+		var Actor = actorChars[ch];
 		  if (Actor)
 			this.actors.push(new Actor(new Vector(x, y), ch));
-		  else if (ch == "x")
-			fieldType = "wall";
-		  else if (ch == "!")
-			fieldType = "lava";
-		  gridLine.push(fieldType);
-		}
-		this.grid.push(gridLine);
-	}
-	
-	
-	
+	});
+	var Actor = actorChars[grid.charatype];
+	this.actors.push(new Actor(grid.startpos));
 }
 
 //元素的位置以及尺寸
@@ -37,9 +27,9 @@ Vector.prototype.times = function(factor) {
 
 //关联活动元素
 var actorChars = {
-  "@": Player,
-  "o": Coin,
-  "=": Lava, "|": Lava
+  "player1": Player,
+  "coin": Coin,
+  "lava1": Lava, "lava2": Lava
 };
 
 //生成玩家类型
@@ -54,9 +44,9 @@ Player.prototype.type = "player";
 function Lava(pos, ch) {
   this.pos = pos;
   this.size = new Vector(1, 1);
-  if (ch == "=") {
+  if (ch == "lava1") {
     this.speed = new Vector(2, 0);
-  } else if (ch == "|") {
+  } else if (ch == "lava2") {
     this.speed = new Vector(0, 2);
 	this.repeatPos = pos;
   }
@@ -72,6 +62,181 @@ function Coin(pos) {
 Coin.prototype.type = "coin";
 
 
-function runGame(){
-	
+
+
+
+
+
+function CanvasDisplay(parent, level) {
+  this.canvas = document.createElement("canvas");
+  this.canvas.width = Math.min(600, level.width * scale);
+  this.canvas.height = Math.min(450, level.height * scale);
+  parent.appendChild(this.canvas);
+  this.cx = this.canvas.getContext("2d");
+
+  this.level = level;
+  this.animationTime = 0;
+  this.flipPlayer = false;
+
+  this.viewport = {
+    left: 0,
+    top: 0,
+    width: this.canvas.width / scale,
+    height: this.canvas.height / scale
+  };
+
+  this.drawFrame(0);
 }
+
+CanvasDisplay.prototype.clear = function() {
+  this.canvas.parentNode.removeChild(this.canvas);
+};
+
+CanvasDisplay.prototype.drawFrame = function(step) {
+  this.animationTime += step;
+
+  this.updateViewport();
+  this.clearDisplay();
+  this.drawBackground();
+  this.drawActors();
+};
+
+CanvasDisplay.prototype.updateViewport = function() {
+  var view = this.viewport, margin = view.width / 3;
+  var player = this.level.player;
+  var center = player.pos.plus(player.size.times(0.5));
+
+  if (center.x < view.left + margin)
+    view.left = Math.max(center.x - margin, 0);
+  else if (center.x > view.left + view.width - margin)
+    view.left = Math.min(center.x + margin - view.width,
+                         this.level.width - view.width);
+  if (center.y < view.top + margin)
+    view.top = Math.max(center.y - margin, 0);
+  else if (center.y > view.top + view.height - margin)
+    view.top = Math.min(center.y + margin - view.height,
+                        this.level.height - view.height);
+};
+
+CanvasDisplay.prototype.clearDisplay = function() {
+  if (this.level.status == "won")
+    this.cx.fillStyle = "rgb(68, 191, 255)";
+  else if (this.level.status == "lost")
+    this.cx.fillStyle = "rgb(44, 136, 214)";
+  else
+    this.cx.fillStyle = "rgb(52, 166, 251)";
+  this.cx.fillRect(0, 0,
+                   this.canvas.width, this.canvas.height);
+};
+
+var otherSprites = document.createElement("img");
+otherSprites.src = "img/sprites.png";
+
+CanvasDisplay.prototype.drawBackground = function() {
+  var view = this.viewport;
+  var xStart = Math.floor(view.left);
+  var xEnd = Math.ceil(view.left + view.width);
+  var yStart = Math.floor(view.top);
+  var yEnd = Math.ceil(view.top + view.height);
+
+  for (var y = yStart; y < yEnd; y++) {
+    for (var x = xStart; x < xEnd; x++) {
+      var tile = this.level.grid[y][x];
+      if (tile == null) continue;
+      var screenX = (x - view.left) * scale;
+      var screenY = (y - view.top) * scale;
+      var tileX = tile == "lava" ? scale : 0;
+      this.cx.drawImage(otherSprites,
+                        tileX,         0, scale, scale,
+                        screenX, screenY, scale, scale);
+    }
+  }
+};
+
+var playerSprites = document.createElement("img");
+playerSprites.src = "img/player.png";
+var playerXOverlap = 4;
+
+CanvasDisplay.prototype.drawPlayer = function(x, y, width,
+                                              height) {
+  var sprite = 8, player = this.level.player;
+  width += playerXOverlap * 2;
+  x -= playerXOverlap;
+  if (player.speed.x != 0)
+    this.flipPlayer = player.speed.x < 0;
+
+  if (player.speed.y != 0)
+    sprite = 9;
+  else if (player.speed.x != 0)
+    sprite = Math.floor(this.animationTime * 12) % 8;
+
+  this.cx.save();
+  if (this.flipPlayer)
+    flipHorizontally(this.cx, x + width / 2);
+
+  this.cx.drawImage(playerSprites,
+                    sprite * width, 0, width, height,
+                    x,              y, width, height);
+
+  this.cx.restore();
+};
+
+CanvasDisplay.prototype.drawActors = function() {
+  this.level.actors.forEach(function(actor) {
+    var width = actor.size.x * scale;
+    var height = actor.size.y * scale;
+    var x = (actor.pos.x - this.viewport.left) * scale;
+    var y = (actor.pos.y - this.viewport.top) * scale;
+    if (actor.type == "player1") {
+      this.drawPlayer(x, y, width, height);
+    } else {
+      var tileX = (actor.type == "coin" ? 2 : 1) * scale;
+      this.cx.drawImage(otherSprites,
+                        tileX, 0, width, height,
+                        x,     y, width, height);
+    }
+  }, this);
+};
+
+
+function runLevel(level, Display, andThen) {
+  var display = new Display(document.body, level);
+  runAnimation(function(step) {
+    level.animate(step, arrows);
+    display.drawFrame(step);
+
+    if(navigator.userAgent.indexOf("MSIE")>0){
+      document.onkeydown = function(){
+        if(event.keyCode == 27){
+          alert('Click and continue game.');
+        }
+      }
+    }
+    else{
+      window.onkeydown = function(){
+        if(event.keyCode == 27){
+          alert('Click and continue game.');
+        }
+      }
+    }
+
+    if (level.isFinished()) {
+      display.clear();
+      if (andThen)
+        andThen(level.status);
+      return false;
+    }
+  });
+}
+
+function runGame(grid,Display){
+	runLevel(new Level(grid), Display, function(status) {
+      if (status == "lost")
+      {
+        alert("Game Over!");
+      }
+      else
+        alert("You win!");
+    });
+}
+
